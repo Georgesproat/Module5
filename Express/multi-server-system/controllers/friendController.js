3302d84cb8b75a71df7f08056c002a959c66f04f
Georgesproat <georgesproat1@gmail.com>
2023-08-18 12:26:16 +1200

    controller Route clean up

--- Express/multi-server-system/controllers/friendController.js
+++ Express/multi-server-system/controllers/friendController.js
@@ -7,54 +7,119 @@ const friends = [
   { id: 6, name: "Rachel", gender: "female" }
 ];
 
-function getFriends() {
-  return friends;
+function getAllFriends(req, res) {
+  const allFriends = friends;
+  res.json(allFriends);
 }
 
-function filterFriendsByGender(gender) {
-  return friends.filter((friend) => friend.gender === gender);
-}
+function filterFriendsByGender(req, res) {
+  const filterGender = req.query.gender;
 
-function findFriendById(id) {
-  const parsedId = parseInt(id); 
-  return friends.find((friend) => friend.id === parsedId);
-}
-
-function addFriend(newFriend) {
-  if (!newFriend.name || !newFriend.gender) {
-    throw new Error("Friend object must contain a name and gender");
+  if (!filterGender) {
+    return res.status(400).json({ error: "Gender is required." });
   }
 
-  newFriend.id = friends.length + 1;
-  friends.push(newFriend);
+  const filteredFriends = friends.filter(
+    (friend) => friend.gender === filterGender
+  );
 
-  return newFriend;
+  if (filteredFriends.length === 0) {
+    return res
+      .status(404)
+      .json({ error: "No friends with the specified gender found." });
+  }
+
+  res.status(200).json(filteredFriends);
 }
 
-function updateFriendById(id, updatedData) {
-  const friendIndex = friends.findIndex((friend) => friend.id === id);
+function filterFriendsByLetter(req, res) {
+  const startsWithLetter = req.query.letter;
+
+  if (!startsWithLetter) {
+    return res.status(400).json({ error: "A letter is required." });
+  }
+
+  const filteredFriends = friends.filter((friend) =>
+    friend.name.startsWith(startsWithLetter)
+  );
+
+  if (filteredFriends.length === 0) {
+    return res.status(404).json({
+      error: "No friends with names starting with the specified letter found."
+    });
+  }
+
+  res.status(200).json(filteredFriends);
+}
+
+function getRequestInfo(req, res) {
+  const info = {
+    "User-Agent": req.headers["user-agent"],
+    "Content-Type": req.headers["content-type"],
+    Host: req.headers["host"]
+  };
+
+  res.json(info);
+}
+
+function getFriendById(req, res) {
+  const friendId = req.params.id;
+  const matchedFriend = friends.find(
+    (friend) => friend.id === parseInt(friendId)
+  );
+
+  if (matchedFriend) {
+    res.status(200).json(matchedFriend);
+  } else {
+    res
+      .status(404)
+      .json({ error: "Friend with id " + friendId + " does not exist" });
+  }
+}
+
+function createNewFriend(req, res) {
+  try {
+    const newFriend = req.body;
+    if (!newFriend.name || !newFriend.gender) {
+      throw new Error("Friend object must contain a name and gender");
+    }
+
+    newFriend.id = friends.length + 1;
+    friends.push(newFriend);
+
+    res.status(201).json(newFriend);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+function updateFriend(req, res) {
+  const friendId = parseInt(req.params.id);
+  const updatedFriendData = req.body;
+
+  const friendIndex = friends.findIndex((friend) => friend.id === friendId);
 
   if (friendIndex === -1) {
-    return null;
+    return res.status(404).json({ error: "Friend not found" });
   }
 
   const updatedFriend = {
     ...friends[friendIndex],
-    ...updatedData,
-    id: friends[friendIndex].id 
+    ...updatedFriendData,
+    id: friendId
   };
 
   friends[friendIndex] = updatedFriend;
 
-  return updatedFriend;
+  res.status(200).json(updatedFriend);
 }
 
-
-
 module.exports = {
-  getFriends,
+  getAllFriends,
   filterFriendsByGender,
-  findFriendById,
-  addFriend,
-  updateFriendById
+  filterFriendsByLetter,
+  getRequestInfo,
+  getFriendById,
+  createNewFriend,
+  updateFriend
 };
--- Express/multi-server-system/routes/friendRoutes.js
+++ Express/multi-server-system/routes/friendRoutes.js
@@ -2,105 +2,20 @@ const express = require("express");
 const router = express.Router();
 const friendController = require("../controllers/friendController");
 
-// default endpoint, gets all friends
-router.get("/", (req, res) => {
-  const allFriends = friendController.getFriends();
-  res.json(allFriends);
-});
+router.get("/", friendController.getAllFriends);
 
-// filter endpoint, gets friends matching the gender from 'gender' query parameter ie. /friends/filter?gender=male
-router.get("/filter", (req, res) => {
-  let filterGender = req.query.gender;
+router.get("/filter", friendController.filterFriendsByGender);
 
-  if (!filterGender) {
-    return res.status(400).json({ error: "Gender parameter is required." });
-  }
+router.get("/startsWith", friendController.filterFriendsByLetter);
 
-  const filteredFriends = friendController.filterFriendsByGender(filterGender);
+router.get("/info", friendController.getRequestInfo);
 
-  if (filteredFriends.length === 0) {
-    return res
-      .status(404)
-      .json({ error: "No friends with the specified gender found." });
-  }
+router.get("/:id", friendController.getFriendById);
 
-  res.status(200).json(filteredFriends);
-});
+router.post("/", friendController.createNewFriend);
 
-// startsWith endpoint - complete this to get friends whose name starts with a given 'letter' from a query parameter
-router.get("/startsWith", (req, res) => {
-  let startsWithLetter = req.query.letter;
+router.put("/:id", friendController.updateFriend);
 
-  if (!startsWithLetter) {
-    return res.status(400).json({ error: "Letter parameter is required." });
-  }
-
-  const filteredFriends =
-    friendController.filterFriendsByLetter(startsWithLetter);
-
-  if (filteredFriends.length === 0) {
-    return res.status(404).json({
-      error: "No friends with names starting with the specified letter found."
-    });
-  }
-
-  res.status(200).json(filteredFriends);
-});
-
-// gets information about this request from the headers
-router.get("/info", (req, res) => {
-  const info = {
-    "User-Agent": req.headers["user-agent"],
-    "Content-Type": req.headers["content-type"],
-    Host: req.headers["host"]
-  };
-
-  res.json(info);
-});
-
-// dynamic request param endpoint - get the friend matching the specific ID ie. /friends/3
-router.get("/:id", (req, res) => {
-  let friendId = req.params.id;
-  let matchedFriend = friendController.findFriendById(friendId);
-
-  if (matchedFriend) {
-    res.status(200).json(matchedFriend);
-  } else {
-    res
-      .status(404)
-      .json({ error: "Friend with id " + friendId + " does not exist" });
-  }
-});
-
-// a POST request with data sent in the body of the request, representing a new friend to add to our list
-router.post("/", (req, res) => {
-  try {
-    const newFriend = friendController.addFriend(req.body);
-    res.status(201).json(newFriend);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-// a PUT request which will update data for an existing friend
-
-router.put("/:id", (req, res) => {
-  let friendId = parseInt(req.params.id);
-  let updatedFriendData = req.body;
-
-  const updatedFriend = friendController.updateFriendById(
-    friendId,
-    updatedFriendData
-  );
-
-  if (!updatedFriend) {
-    return res.status(404).json({ error: "Friend not found" });
-  }
-
-  res.status(200).json(updatedFriend);
-});
-
-// Catch-all route for undefined endpoints
 router.get("*", (req, res) => {
   res.status(404).json({ error: "Endpoint not found." });
 });
